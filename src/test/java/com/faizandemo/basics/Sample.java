ff27574fd416f2005fa5a919c796094a83f7cd19
NurulhasanF95 <hasanfaizan4ever@gmail.com>
2021-04-05 11:29:06 +0530

    Second commit

--- src/test/java/com/faizandemo/basics/Sample.java
+++ src/test/java/com/faizandemo/basics/Sample.java
@@ -1,5 +1,44 @@
 package com.faizandemo.basics;
 
-public class Sample {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Sample 
+{
+
+	WebDriver driver;
+	@BeforeTest
+	public void setUp()
+	{
+		WebDriverManager.chromedriver().setup();
+		
+		driver=new ChromeDriver();
+		
+		driver.get("http://www.fb.com");
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void verifytitle()
+	{
+		Assert.assertEquals(driver.getTitle(), "Facebook");
+	}
+	
+	@AfterTest
+	public void tearDown()
+	{
+		driver.close();
+	}
+	
 }
